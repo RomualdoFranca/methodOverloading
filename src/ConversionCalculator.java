0d9ce1458487b13f99c6e4e46998d57fcacf2bf9
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-06 18:10:50 -0300

    Created formula into control statemets

--- src/ConversionCalculator.java
+++ src/ConversionCalculator.java
@@ -2,16 +2,17 @@ public class ConversionCalculator {
 
     public static void main(String[] args) {
 
-        calfeetAndToCentimeters(-4,5);
-        double validation = calfeetAndToCentimeters(4,-5);
-        System.out.println(validation);
+        calfeetAndToCentimeters(10, 10);
+        double validation = calfeetAndToCentimeters(10,10);
+        System.out.printf("%.2f", validation);
     }
     public static double calfeetAndToCentimeters(double feet, double inches) {
         if (feet < 0 || inches <0 || inches > 12) {
             return -1;
-        } else {
-            System.out.println("The parameters are valids");
         }
-        return -1;
+        System.out.println(feet + " Feets " +  inches + " inches " );
+        inches *= 2.54;
+        feet = inches * 12;
+        return inches + feet ;
     }
 }
