14306570d8c308fd0140643ed11d4a5630e67ac0
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-06 20:19:57 -0300

    Aplicaçao incompleta.

--- src/ConversionCalculator.java
+++ src/ConversionCalculator.java
@@ -2,25 +2,26 @@ public class ConversionCalculator {
 // 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
     public static void main(String[] args) {
 
-        calfeetAndToCentimeters(10, 10);
-        double validation = calfeetAndToCentimeters(10, 10);
-        System.out.printf("%.2f\n", validation);
+//        calfeetAndToCentimeters(10, 10);
+//        double validation = calfeetAndToCentimeters(10, 10);
+//        System.out.printf("%.2f\n", validation);
 
         double overLoading1 = calfeetAndToCentimeters(12);
-        System.out.printf("Transformed inches in centimeters: %.2fcm\n ", overLoading1);
+//        System.out.printf("Transformed inches in centimeters: %.2fcm\n ", overLoading1);
 
         double inches = overLoading1 / 2.54;
 //        inches /= 2.54;
-        System.out.printf("Transformed centimeters in inches: %.2fin\n ", inches);
+//        System.out.printf("Transformed centimeters in inches: %.2fin\n ", inches);
 
-        double feets = overLoading1 / inches;
+        double feets = overLoading1 / 30.48;
 //        feets /= 12;
-        System.out.printf("Transformed centimeters in feets: %.2fft\n ", feets);
+//        System.out.printf("Transformed centimeters in feets: %.2fft\n ", feets);
 
-        double reverseConversion =  calfeetAndToCentimeters(inches, feets);
-        System.out.println(reverseConversion);
+        double reverseConversion =  calfeetAndToCentimeters(feets, inches);
+        System.out.printf("%.2f", reverseConversion);
     }
-
+// test for validation to check if calculation are correct
+    //checking parameters
     public static double calfeetAndToCentimeters(double feet, double inches) {
         if (feet < 0 || inches < 0 || inches > 12) {
             return -1;
@@ -36,7 +37,6 @@ public class ConversionCalculator {
         if (inches < 0) {
             return -1;
         }
-//        return inches / 12;
-        return inches * 2.54;
+        calfeetAndToCentimeters(1, 12);
     }
 }
