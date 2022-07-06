ced4fb84b375e241d10c5fb6661d17b79dd2d50e
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-06 19:31:10 -0300

    Refazendo o commit anterior

--- src/ConversionCalculator.java
+++ src/ConversionCalculator.java
@@ -1,18 +1,42 @@
 public class ConversionCalculator {
-
+// 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
     public static void main(String[] args) {
 
         calfeetAndToCentimeters(10, 10);
-        double validation = calfeetAndToCentimeters(10,10);
-        System.out.printf("%.2f", validation);
+        double validation = calfeetAndToCentimeters(10, 10);
+        System.out.printf("%.2f\n", validation);
+
+        double overLoading1 = calfeetAndToCentimeters(12);
+        System.out.printf("Transformed inches in centimeters: %.2fcm\n ", overLoading1);
+
+        double inches = overLoading1 / 2.54;
+//        inches /= 2.54;
+        System.out.printf("Transformed centimeters in inches: %.2fin\n ", inches);
+
+        double feets = overLoading1 / inches;
+//        feets /= 12;
+        System.out.printf("Transformed centimeters in feets: %.2fft\n ", feets);
+
+        double reverseConversion =  calfeetAndToCentimeters(inches, feets);
+        System.out.println(reverseConversion);
     }
+
     public static double calfeetAndToCentimeters(double feet, double inches) {
-        if (feet < 0 || inches <0 || inches > 12) {
+        if (feet < 0 || inches < 0 || inches > 12) {
             return -1;
         }
-        System.out.println(feet + " Feets " +  inches + " inches " );
+        System.out.printf("%.2f feet and %.2f inches \n", feet, inches);
         inches *= 2.54;
         feet = inches * 12;
-        return inches + feet ;
+        return inches + feet;
+    }
+
+    public static double calfeetAndToCentimeters(double inches) {
+
+        if (inches < 0) {
+            return -1;
+        }
+//        return inches / 12;
+        return inches * 2.54;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,29 @@ public class Main {
 
         // Observar a diferença de output entre chamar apenas o metodo ou criar uma variavel para armezanar o valor
         // retornado do metodo
+        //  Dessa forma apenas os valores dos parametros sao mostrados
         calculateScore("Mamão", 40);
 
-        // Essa variavel armazena o valor do retorno (score * 1000)
+        //  Dessa forma tanto os valores dos parametros sao mostrados como o valor do retorno
         int playerScore =  calculateScore("Mamão", 40);
         System.out.println(playerScore);
+
+        calculateScore(349);//  apenas o valor do score
+        int finalScore = calculateScore(349);
+        System.out.println("Final score: " + finalScore); // Valor inserido no parametro e o valor de retorno
+        calculateScore();
     }
     public static int calculateScore(String playerName, int score) {
         // Esse output apenas imprime os valores dos parametros nao retorna o resultado da multiplicacao
-        System.out.printf("%s scored %d\n", playerName, score);
+        System.out.printf("%s scored %d total = ", playerName, score);
         return score * 1000;
     }
     public static int calculateScore(int score) {
-        System.out.printf(" scored %d\n", score);
+        System.out.printf("scored %d\n", score);
         return score * 1000;
+    }
+    public static int calculateScore() {
+        System.out.println("No player name no player score.");
+        return 0;
     }
 }
