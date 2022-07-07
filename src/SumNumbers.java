ba5b1991f57f8fe70b9d87286a93d653b3f6cd8c
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-07 18:20:37 -0300

    Cria outro exemplode metodo overloading

--- src/SumNumbers.java
+++ src/SumNumbers.java
@@ -1,2 +1,18 @@
 public class SumNumbers {
+    public static void main(String[] args) {
+
+    }
+    //  É possivel acrescentar ou diminuir paramentros nos overloading.
+    //  Pode-se usar um parametro dentro do outro
+    // Pode-se chamar um metodo similar, mas com diferentes tipos de dados
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+    public static int sum(int num1, int num2, int num3) { // 1° Overloading
+        return num1 + num2 + num3;
+    }
+    public static int sum(int num1, int num2, int num3, int num4) { // 2° Overloading
+        return num1 + num2 + num3 + num4;
+    }
+
 }
